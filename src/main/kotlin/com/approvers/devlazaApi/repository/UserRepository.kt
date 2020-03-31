0206f19876e04da4750c59a2488fbb05b14b4f8b
isso0424 <isso04240129@gmail.com>
2020-03-31 19:15:28 +0900

    [Add]メールの認証トークンのリポジトリを追加

--- src/main/kotlin/com/approvers/devlazaApi/repository/UserRepository.kt
+++ src/main/kotlin/com/approvers/devlazaApi/repository/UserRepository.kt
@@ -1,9 +1,6 @@
 package com.approvers.devlazaApi.repository
 
-import com.approvers.devlazaApi.model.DevelopExp
-import com.approvers.devlazaApi.model.Follow
-import com.approvers.devlazaApi.model.Token
-import com.approvers.devlazaApi.model.User
+import com.approvers.devlazaApi.model.*
 import org.springframework.data.jpa.repository.JpaRepository
 import java.util.*
 
@@ -11,6 +8,8 @@ interface UserRepository: JpaRepository<User, String>{
     fun findByName(name: String): List<User>
     fun findByNameLike(name: String): List<User>
     fun findById(id: UUID): List<User>
+    fun findByShowId(id: String): List<User>
+    fun findByMailAddress(address: String): List<User>
 }
 
 interface TokenRepository: JpaRepository<Token, UUID>{
@@ -24,4 +23,8 @@ interface DevelopExpRepository: JpaRepository<DevelopExp, UUID>{
 interface FollowRepository : JpaRepository<Follow, UUID> {
     fun findByUserId(userId: UUID): List<Follow>
     fun findByFollowingUserId(userId: UUID): List<Follow>
+}
+
+interface MailTokenRepository: JpaRepository<MailToken, UUID> {
+    fun findByToken(token: String): List<MailToken>
 }
