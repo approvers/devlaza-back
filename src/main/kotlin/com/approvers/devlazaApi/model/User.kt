86782db1103c1725ac7ba81cc4329da8e50749e2
isso0424 <isso04240129@gmail.com>
2020-04-08 01:24:39 +0900

    [Fix]FollowのcolumnListとColumnをuser_idに変更

--- src/main/kotlin/com/approvers/devlazaApi/model/User.kt
+++ src/main/kotlin/com/approvers/devlazaApi/model/User.kt
@@ -41,10 +41,10 @@ data class DevelopExp(
 )
 
 @Entity
-@Table(indexes = [Index(columnList="USERID, following_user_id", unique=true)])
+@Table(indexes = [Index(columnList="user_id, following_user_id", unique=true)])
 data class Follow(
 		@Id @GeneratedValue(generator="uuid2") @GenericGenerator(name="uuid2", strategy="uuid2") @Column(columnDefinition="BINARY(16)") var id: UUID? = null,
-		@Column(name="userId") var userId: UUID,
+		@Column(name="user_id") var userId: UUID,
 		@Column(name="following_user_id") var followingUserId: UUID
 )
 
