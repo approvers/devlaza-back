442c6592104b5e1d0c008c8e6161ca6fa078454a
mirror-kt <me@mirror-kt.dev>
2020-04-08 02:00:44 +0900

    add ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,4 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-import org.jlleitschuh.gradle.ktlint.reporter.ReporterType
 
 plugins {
     id("org.springframework.boot") version "2.2.6.RELEASE"
@@ -7,9 +6,6 @@ plugins {
     kotlin("jvm") version "1.3.71"
     kotlin("plugin.spring") version "1.3.71"
     kotlin("plugin.jpa") version "1.3.71"
-
-    id("org.jlleitschuh.gradle.ktlint") version "9.2.1"
-    id("com.github.unchai.ktlint-github") version "0.0.1"
 }
 
 group = "com.approvers"
@@ -51,18 +47,4 @@ tasks.withType<KotlinCompile> {
         freeCompilerArgs = listOf("-Xjsr305=strict")
         jvmTarget = "1.8"
     }
-}
-
-ktlint {
-    verbose.set(true)
-    outputToConsole.set(true)
-    coloredOutput.set(true)
-    ignoreFailures.set(true)
-    reporters {
-        reporter(ReporterType.CHECKSTYLE)
-    }
-}
-
-ktlintGithub {
-    ghRepository = "brokenManager/devlaza-back"
 }
