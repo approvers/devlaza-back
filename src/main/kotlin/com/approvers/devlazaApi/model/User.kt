6a0cae75f2074a221c1835da4f240482da4cf3b9
Kota Isozaki <49902177+isso0424@users.noreply.github.com>
2020-04-08 01:14:37 +0900

    Update src/main/kotlin/com/approvers/devlazaApi/model/User.kt
    Co-Authored-By: Misato Kano <kyoutoday@kyou-today.dev>

--- src/main/kotlin/com/approvers/devlazaApi/model/User.kt
+++ src/main/kotlin/com/approvers/devlazaApi/model/User.kt
@@ -36,7 +36,7 @@ data class Token(
 @Table(indexes = [Index(name="develop_exp_index", columnList="USERID", unique=true)])
 data class DevelopExp(
 		@Id @GeneratedValue(generator="uuid2") @GenericGenerator(name="uuid2", strategy="uuid2") @Column(columnDefinition="BINARY(16)") var id: UUID? = null,
-		@Column(name="userId") var userId: UUID,
+		@Column(name="user_id") var userId: UUID,
 		@Column(name="caption") var caption: String
 )
 
