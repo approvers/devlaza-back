f370c210f374a1179900b94824187f232eb95872
mirror-kt <me@mirror-kt.dev>
2020-04-09 01:25:51 +0900

    check to review

--- src/test/kotlin/com/approvers/devlazaApi/Apiprac2ApplicationTests.kt
+++ src/test/kotlin/com/approvers/devlazaApi/Apiprac2ApplicationTests.kt
@@ -1,6 +1,6 @@
 package com.approvers.devlazaApi
 
-//import org.junit.jupiter.api.Test
+// import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
