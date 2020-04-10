7a01245dced8485c6d4092509d5844951882cafb
isso0424 <isso04240129@gmail.com>
2020-04-11 02:43:52 +0900

    [Fix]Linter君ごめん

--- src/test/kotlin/com/approvers/devlazaApi/Apiprac2ApplicationTests.kt
+++ src/test/kotlin/com/approvers/devlazaApi/Apiprac2ApplicationTests.kt
@@ -2,12 +2,9 @@ package com.approvers.devlazaApi
 
 // import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.TestTemplate
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.boot.test.web.client.TestRestTemplate
-import org.springframework.boot.test.web.client.getForEntity
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
