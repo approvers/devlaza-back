e99ce0c9de0195465171459d67788c29eba3375f
isso0424 <isso04240129@gmail.com>
2020-04-12 17:33:44 +0900

    [Fix]エラーハンドラが機能せずに500エラーになってたのを修正

--- src/main/kotlin/com/approvers/devlazaApi/errors/ErrorResponseHandler.kt
+++ src/main/kotlin/com/approvers/devlazaApi/errors/ErrorResponseHandler.kt
@@ -3,6 +3,7 @@ package com.approvers.devlazaApi.errors
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
 import org.springframework.web.context.request.WebRequest
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler
@@ -21,5 +22,23 @@ class ErrorResponseHandler : ResponseEntityExceptionHandler() {
             body
         }
         return ResponseEntity(returnBody, headers, status)
+    }
+
+    @ExceptionHandler(NotFound::class)
+    fun handle404(ex: NotFound, request: WebRequest): ResponseEntity<Any> {
+        val headers = HttpHeaders()
+        val body = ErrorContents("NotFound", ex.message!!, "404")
+        val status = HttpStatus.NOT_FOUND
+
+        return handleExceptionInternal(ex, body, headers, status, request)
+    }
+
+    @ExceptionHandler(BadRequest::class)
+    fun handle400(ex: BadRequest, request: WebRequest): ResponseEntity<Any> {
+        val headers = HttpHeaders()
+        val body = ErrorContents("BadRequest", ex.message!!, "400")
+        val status = HttpStatus.BAD_REQUEST
+
+        return handleExceptionInternal(ex, body, headers, status, request)
     }
 }
